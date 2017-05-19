c35fc0ecf675512cf96d27c7ddf82415fb3c8fc8
James Tobin <jtobin@mitre.org>
2017-05-19 13:20:31 -0400

    Pulls random word from endpoint, has picture for hosting, and gets data validation for captcha.  Still needs captcha validation algorithm

--- Server.rb
+++ Server.rb
@@ -7,6 +7,7 @@ get '/' do
 end
 
 get '/home' do
+  @qid = Random.rand(70)
   erb :home
 end
 
@@ -43,14 +44,14 @@ end
 __END__
 
 @@ home
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
     <title>CAPTTTTCHA</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript">
         $(function () {
-            $('#viz_form').submit(function (ev) {
+            $('#captcha').submit(function (ev) {
                 //this happens if form is submitted
                 //prevent the default behavior of a form (it should do nothing in our case)
                 ev.preventDefault();
@@ -58,33 +59,32 @@ __END__
 
                 $.ajax({
                     url: "/captcha/validate",
-                    //serialize the form and use it as data for our ajax request
+                    //serialize the form and use it as data for ajax request
                     data: $(this).serialize(),
-                    //the type of data we are expecting back from server, could be json too
                     dataType: "html",
                     success: function (data) {
-                        //if our ajax request is successful, replace the content of our viz div with the response data
-                        $('#viz').html(data);
+                        //if our ajax request is successful, replace the content of our output div with the response data
+                        $('#output').html(data);
                     }
                 });
-                //show the link to the repo_viz (don't forget: we are still in the submit event!)
-                //$('#viz_link').append($("#viz_form input:first").val());
-                $('#viz_p').show();
             });
 
         });
     </script>
     <script type="text/javascript">
         function load() {
-            var all_checkbox_divs = document.getElementsByClassName("image-checkbox");
+            //Gets all areas in the map
+            var all_checkbox_divs = document.getElementsByTagName("AREA");
 
             for (var i=0;i<all_checkbox_divs.length;i++) {
 
+                //adds onclick functions to all areas
                 all_checkbox_divs[i].onclick = function (e) {
                     var div_id = this.id;
                     var checkbox_id =div_id.split("_")[0];
                     var checkbox_element = document.getElementById(checkbox_id);
 
+                    //toggles hidden checkboxes when areas are clicked
                     if (checkbox_element.checked === true) {
                         checkbox_element.checked = false;
                         this.setAttribute("class","image-checkbox");
@@ -95,52 +95,53 @@ __END__
 
                 };
             }
+            RandomWord();
 
+        }
+    </script>
+    <script>
+        function RandomWord() {
+            //query random word generator to get random word
+            var requestStr = "http://setgetgo.com/randomword/get.php";
+
+            $.ajax({
+                type: "GET",
+                url: requestStr,
+                dataType: "html",
+                success: function (data) {
+                    //if our ajax request is successful, replace the content of our noun div with the response data
+                    $('#noun').html(data);
+                }
+            });
         }
     </script>
     <style>
         input.cb {
             visibility: hidden;
         }
-
-        .image-checkbox {
-            background:url(http://blog.adriansandu.com/wp-content/uploads/2012/08/ie8-broken.png);
-            width:100px;
-            height:100px;
-        }
-
-        .image-checkbox-checked {
-            background:url(http://blog.adriansandu.com/wp-content/uploads/2012/08/ie8-broken.png);
-            width:100px;
-            height:100px;
-        }
     </style>
 </head>
 <body onload="load()">
-<form action="/" method="post" id="viz_form">
-    <label>Enter your username:</label>
-    <input name="qid" type="hidden" value="9">
+<h4>Please complete the CAPTCHA below to receive your <b>FREE</b> flag.</h4>
+<form action="/" method="post" id="captcha">
+    <input name="qid" type="number" hidden value="<%= @qid %>">
     <input name="cb0" type="checkbox" id="cb0" class="cb"/>
-    <div id="cb0_proxy" class="image-checkbox"/>
+    <img src="images/cap.png" height="400" width="400" usemap="#capPic"/>
+    <map name="capPic">
+        <area id="cb0_proxy" coords="0,0,200,200"/>
+        <area id="cb1_proxy" coords="200,0,400,200"/>
+        <area id="cb2_proxy" coords="0,200,200,400"/>
+        <area id="cb3_proxy" coords="200,200,400,400"/>
+    </map>
     <input name="cb1" type="checkbox" class="cb" id="cb1"/>
-    <div id="cb1_proxy" class="image-checkbox"/>
     <input name="cb2" type="checkbox" class="cb" id="cb2"/>
-    <div id="cb2_proxy" class="image-checkbox"/>
     <input name="cb3" type="checkbox" class="cb" id="cb3"/>
-    <div id="cb3_proxy" class="image-checkbox"/>
-    <!--<label for="cb0"><img src="/bad/link"/></label>
-    <input name="cb1" type="checkbox" class="cb"/>
-    <label for="cb1"><img src="/awful/link"/></label>
-    <input name="cb2" type="checkbox" class="cb"/>
-    <label for="cb2"><img src="/broken/link"/></label>
-    <input name="cb3" type="checkbox" class="cb"/>
-    <label for="cb3"><img src="/no"/></label> -->
-    <br/><br/><br/><br/><br/><br/><br/>
-    <input type="submit" value="Do it!">
+    <br/><br/><br/>
+    Click all the images of <b id="noun"></b>.
+    <br/><br/><br/><br/>
+    <input type="submit" value="Verify">
 </form>
-<p id="viz_p" style="display:none;"><a id="viz_link"
-                                       href="#">click here if you want to see the awesome repo_viz for </a></p>
-<div id="viz">
+<div id="output">
 </div>
 </body>
 </html>
